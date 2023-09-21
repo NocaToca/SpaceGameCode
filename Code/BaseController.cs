689db19a1a52db4cc667df2b33f035a1e468469c
NocaToca <66977504+NocaToca@users.noreply.github.com>
2023-09-21 17:21:51 -0400

    omg I'm learning!
    We have smart pathfinding with fluid movement now. Thanks Godot!

--- Code/BaseController.cs
+++ Code/BaseController.cs
@@ -3,8 +3,8 @@ using System;
 
 public partial class BaseController : Node2D
 {
-
-	private Sprite2D character {get{return GetNode<Sprite2D>("Character");}} 
+	private CharacterBody2D main_node {get{return GetNode<CharacterBody2D>("CharacterBody2D");}}
+	private Sprite2D character {get{return main_node.GetNode<Sprite2D>("Character");}} 
 
 	//Basically, on input, we're going to assess where we're clicking and then move our little icon accordingly.
 	//Keep in mind this game is going to be a top down, so we're going to actually do a click-based movement
@@ -12,7 +12,7 @@ public partial class BaseController : Node2D
 	/*
 		-First make basic movement (Just go directly to target)
 		-Make fluid movement (Travel time, movement speed)
-		-Make idea for smart movment (avoids obstacles, goes the quickest route)
+		-Make idea for smart movment (avoids obstacles, goes the quickest route) - Wow! NavigationServer seems very powerful. Let us implement that
 		-Refine smart movement
 	*/
 
@@ -21,16 +21,54 @@ public partial class BaseController : Node2D
 
 	//Tween for animation
 	Tween movement_tween;
-	float movement_speed = 0.5f; //(m/s)
+	float movement_speed = 25.0f; //(m/s)
+	private Vector2 destination_vector; 
+
+	//Okay, now we have the Tween, let's add the navigation mesh!
+	private NavigationAgent2D navigation_agent;
+	private bool started = false;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready(){
 		//AddChild(movement_tween);
+		//We need to initialize the agent here!
+		navigation_agent = main_node.GetNode<NavigationAgent2D>("NavigationAgent2D");
+		destination_vector = character.Position;
+
+		navigation_agent.PathDesiredDistance = 4.0f;
+		navigation_agent.TargetDesiredDistance = 4.0f;
+
+		Callable.From(ActorSetup).CallDeferred();
+	}
+
+	//We need to wait for the physic frame to set up to sync
+	private async void ActorSetup(){
+		await ToSignal(GetTree(), SceneTree.SignalName.PhysicsFrame);
+
+		started = true;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
+	public override void _Process(double delta){
+	}
+
+	//I don't know why they do it in physics process instead of process (Maybe to do it before colliders?)
+	//Actually I think it's because NavigationAgent2D.GetNextPosition() runs on the physics threads
+	public override void _PhysicsProcess(double delta){
+		if(!started || navigation_agent.IsNavigationFinished()){
+			return; //We don't want to do anything if we're at the destination!
+		}
+
+		MoveCharacterNavMesh(navigation_agent.GetNextPathPosition());
+
+	}
+
+	//Moves the character relative to the nav mesh!
+	private void MoveCharacterNavMesh(Vector2 to_position){
+		Vector2 velocity = (to_position - character.GlobalTransform.Origin).Normalized();
+		velocity *= movement_speed;
+		main_node.Velocity = velocity;
+		main_node.MoveAndSlide();
 	}
 
 	//Called for inputs
@@ -40,10 +78,15 @@ public partial class BaseController : Node2D
 		if(@event is InputEventMouseButton mouse_button_event){
 			if(mouse_button_event.Pressed && mouse_button_event.ButtonIndex == MouseButton.Right){
 				//Right button has been clicked
-				MoveCharacter(mouse_button_event.GlobalPosition);
+				SetDestination(mouse_button_event.GlobalPosition);
 
 			}
 		}
+	}
+
+	public void SetDestination(Vector2 destination){
+		destination_vector = destination;
+		navigation_agent.TargetPosition = destination;
 	}
 
 	//Uses our movement speed to grab the amount of time we need
@@ -53,7 +96,7 @@ public partial class BaseController : Node2D
 		return distance/movement_speed;
 	}
 
-	public void MoveCharacter(Vector2 global_position){
+	public void MoveCharacter(Vector2 to_position){
 		//character.Position = global_position; //Wow! But this snaps to a position, not allowing us to have fluid movement :c
 		if(movement_tween != null){
 			movement_tween.Kill();
@@ -61,7 +104,7 @@ public partial class BaseController : Node2D
 		//Something I read about were Tweens, so let's try to use that. Apparently they're great for interpolation!
 		movement_tween = GetTree().CreateTween();
 
-		movement_tween.TweenProperty(character, "position", global_position, 1.0f);
+		movement_tween.TweenProperty(character, "position", to_position, 1.0f);
 
 
 	}
